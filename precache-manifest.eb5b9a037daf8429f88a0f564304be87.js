41de041833cd3d2e9bd7ca3ea3ef4f967e704376
ardian <reijavick@gmail.com>
2018-11-20 07:32:48 +0000

    Updates

---
+++ precache-manifest.eb5b9a037daf8429f88a0f564304be87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "333e0a0305ca569b3bd3",
+    "url": "/fe-web-test-shopee/static/js/runtime~main.333e0a03.js"
+  },
+  {
+    "revision": "014394810b387fa0d589",
+    "url": "/fe-web-test-shopee/static/js/main.01439481.chunk.js"
+  },
+  {
+    "revision": "7b801066bf4e44d42d6d",
+    "url": "/fe-web-test-shopee/static/js/1.7b801066.chunk.js"
+  },
+  {
+    "revision": "014394810b387fa0d589",
+    "url": "/fe-web-test-shopee/static/css/main.08f0376d.chunk.css"
+  },
+  {
+    "revision": "4437308d0529881f646ca37af5c55782",
+    "url": "/fe-web-test-shopee/index.html"
+  }
+];
